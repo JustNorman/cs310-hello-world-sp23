4e084394e50b57739106d1939cc4773294c3a725
JustNorman <norman2and2@gmail.com>
2023-01-22 12:18:04 -0600

    added my name added f2()-f4()

--- Class2.java
+++ Class2.java
@@ -1,12 +1,20 @@
 /**
  *
- * @author Your Name
+ * @author Martell Norman
  */
 
 public class Class2 {
 
     private void f1() {
         // v1.0
+    }
+
+    private void f2() {
+        // v2.0
+    }
+
+    private void f3() {
+        // v3.0
     }
 
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Martell Norman //Change to my Name
  */
 
 public class HelloWorld {
@@ -13,6 +13,14 @@ public class HelloWorld {
 
     private void f1() {
         // v1.0
+    }
+
+    private void f2() {
+        // v2.0
+    }
+
+    private void f3() {
+        // v3.0
     }
 
 }
